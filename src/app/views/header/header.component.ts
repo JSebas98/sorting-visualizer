b379c282e4759c595ab609404168edb5ce43f437
CarlaBarrientos <carla.andrea.cbm@gmail.com>
2022-05-02 12:40:14 -0400

    Fixing some styles

--- src/app/views/header/header.component.ts
+++ src/app/views/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  @Output() selectedOption = new EventEmitter<number>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectOption(option: number) {
+    switch(option) { 
+      case 1: {
+        this.selectedOption.emit(option);
+        break; 
+      } 
+      case 2: { 
+        this.selectedOption.emit(option);
+        break; 
+      }
+      case 3: { 
+        this.selectedOption.emit(option);
+        break; 
+      } 
+      case 4: { 
+        this.selectedOption.emit(option);
+        break; 
+      }  
+      default: { 
+        this.selectedOption.emit(0);
+        break; 
+      } 
+   } 
+  }
 }
