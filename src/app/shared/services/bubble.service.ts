ed53c92c623dd5c5c76a24b728122621e48fe283
Sebastian Beltran <sebas.beltran.98@gmail.com>
2022-05-02 10:50:47 -0500

    Refactor BubbleService to clean logs, return sorted array and store steps in public property

--- src/app/shared/services/bubble.service.ts
+++ src/app/shared/services/bubble.service.ts
@@ -7,21 +7,21 @@ import { Step } from '../models/types';
 
 export class BubbleSorting{
     public arraySize: 'small' | 'medium' | 'big';
-    public rawArray: RawArray;
+    public rawArray: number[];
+    public steps: Step[];
+    public stepsCounter: number;
 
     constructor(arraySize: 'small' | 'medium' | 'big') {
         this.arraySize = arraySize;
-        this.rawArray = new RawArray(arraySize);
+        this.rawArray = new RawArray(arraySize).generateArray(arraySize);
+        this.steps = [];
+        this.stepsCounter = 0;
     }
 
-    bubbleSortArray(rawArray: RawArray): Step[] {
+    bubbleSortArray(arr: number[] = this.rawArray): number[] {
         // Definitions
-        let arr = rawArray.values;
-        console.log('Unsorted array:', arr);
         let i: number, j:number, temp: number;
         let swapped: boolean;
-        let stepsCounter:number = 0;
-        let steps:Step[] = [];
 
         // Outer loop
         for (i = 0; i < arr.length - 1; i++) {
@@ -33,20 +33,20 @@ export class BubbleSorting{
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                     swapped = true;
-                    stepsCounter++;
+                    this.stepsCounter++;
                     //console.log(stepsCounter, arr);
                     // Keep track of each step
-                    steps.push({
-                        "key": stepsCounter,
+                    this.steps.push({
+                        "key": this.stepsCounter,
                         "status":arr.slice(),
                         "pointer": j,
                         "comparedElement": j+1 
                     });
                     //console.log(steps[stepsCounter-1]);
                 } else { // Record step even if no swap has been made
-                    stepsCounter++;
-                    steps.push({
-                        "key": stepsCounter,
+                    this.stepsCounter++;
+                    this.steps.push({
+                        "key": this.stepsCounter,
                         "status":arr.slice(),
                         "pointer": j,
                         "comparedElement": j+1 
@@ -61,10 +61,6 @@ export class BubbleSorting{
             }
         }
 
-        return steps;
+        return arr;
     }
 }
-
-// let bubble1 = new BubbleSorting('small');
-// let steps = bubble1.bubbleSortArray(bubble1.rawArray);
-// console.log(steps);
